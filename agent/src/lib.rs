e564ed578a0674bf925d3c3b2df865a4b118f40c
0xgleb <gleb.dianov@protonmail.com>
2024-07-14 05:20:48 +0200

    compiling plumbing

--- agent/src/lib.rs
+++ agent/src/lib.rs
@@ -1,29 +1,75 @@
+use bigdecimal::{BigDecimal, ToPrimitive, Zero, num_bigint::Sign};
+use std::ops::{Add, Mul, Sub};
 use std::str::FromStr;
-use near_sdk::{env, json_types::U128, log, near, AccountId, Gas, Promise};
+use rand_distr::{Distribution, Pareto};
 
-mod bandit;
-mod token;
+use near_sdk::*;
+use near_sdk::json_types::U128;
+use near_sdk::collections::{LookupMap, TreeMap, Vector};
+
 mod dex;
-
 use dex::*;
+
+mod token;
+use token::*;
+
+// mod bandit;
+// use bandit::*;
 
 // Define the contract structure
 #[near(contract_state)]
+#[derive(PanicOnDefault)]
 pub struct Contract {
     dex_id: AccountId,
-}
-
-// Define the default, which automatically initializes the contract
-impl Default for Contract {
-    fn default() -> Self {
-        let dex_id = AccountId::from_str("ref-finance-101.testnet").unwrap();
-        Self { dex_id }
-    }
+    arms: Vector<BigDeDecimal>,
+    rewards: TreeMap<BigDeDecimal, (BigDeDecimal, BigDeDecimal)>,
+    mean_gain: TreeMap<BigDeDecimal, BigDeDecimal>,
+    inventory: LookupMap<Token, BigDeDecimal>,
+    trade_size: BigDeDecimal,
 }
 
 // Implement the contract structure
 #[near]
 impl Contract {
+    #[init]
+    #[private]
+    pub fn init() -> Self {
+        let dex_id = AccountId::from_str("ref-finance-101.testnet").unwrap();
+
+        let start = BigDecimal::from_str("0.0001").unwrap();
+        let end = BigDecimal::from_str("0.03").unwrap();
+        let num_elements: u32 = 50;
+
+        let step = (end - &start) / BigDecimal::from(num_elements - 1);
+        let trade_size = BigDecimal::from_str("0.1").unwrap().into();
+
+        let mut arms = Vector::new(b"arms".to_vec());
+        let mut rewards = TreeMap::new(b"rewards".to_vec());
+        let mut mean_gain = TreeMap::new(b"mean_gain".to_vec());
+        let mut inventory = LookupMap::new(b"inventory".to_vec());
+
+        // Create initial state
+        inventory.insert(&Token::near(), &BigDecimal::from(200).into());
+
+        let bins = (0..num_elements).map(|i| &start + &step * BigDecimal::from(i.to_u32().unwrap()));
+        for bin in bins {
+            let arm = bin.into();
+            let zero = || BigDecimal::zero().into();
+            arms.push(&arm);
+            mean_gain.insert(&arm, &zero());
+            rewards.insert(&arm, &(zero(), zero()));
+        }
+
+        Self {
+            dex_id,
+            arms,
+            rewards,
+            mean_gain,
+            inventory,
+            trade_size,
+        }
+    }
+
     // Public method - accepts a greeting, such as "howdy", and records it
     pub fn step(&mut self) -> Promise {
         let greeting = "placeholder".to_string(); //
@@ -64,6 +110,55 @@ impl Contract {
 
         "HelloWorld".to_string()
     }
+
+
+    // Takes as input a percentage gain obtained by a 3-hop swap starting and ending in NEAR
+    // denominated in NEAR
+    pub fn tick(&mut self, gain: BigDeDecimal) {
+        let gain = BigDecimal::from(gain);
+        let arm = self.get_arm(gain.clone());
+        let expected_gain: BigDecimal = self.mean_gain.get(&arm.into()).unwrap().into();
+
+        let near = &Token::near();
+
+        let trade_size = self.trade_size.clone();
+        let trade_size = BigDecimal::from(trade_size);
+
+        let near_owned = self.inventory.get(near).unwrap();
+        let near_owned = &BigDecimal::from(near_owned);
+
+        let trained = self.rewards.len() >= 111;
+        let profit_expected = expected_gain.to_f64().unwrap() > 0.0;
+
+        if !trained || profit_expected {
+            let pnl = self.trade(near_owned.mul(trade_size), gain.clone());
+            let near_owned = near_owned.add(pnl);
+            self.inventory.insert(near, &near_owned.into());
+        }
+    }
+
+    fn get_arm(&mut self, gain: BigDecimal) -> BigDecimal {
+        let arms = self.arms.iter().map(|arm| BigDecimal::from(arm.to_owned())).collect::<Vec<BigDecimal>>();
+        let last = arms.last().unwrap();
+        let found = arms.iter().find(|x| **x >= gain);
+        found.unwrap_or(last).to_owned()
+    }
+
+    fn trade(&self, amount: BigDecimal, expected_gain: BigDecimal) -> BigDecimal {
+        // Placeholder for trading logic
+        let scale = 1.0; // 'xm' parameter in the Pareto distribution
+        let shape = 1.5; // 'alpha' parameter in the Pareto distribution
+        // Create a Pareto distribution with the given scale and shape parameters
+        let pareto = Pareto::new(scale, shape).unwrap();
+
+        // Create a random number generator
+        let mut rng = rand::thread_rng();
+        let gas = BigDecimal::from_str(& pareto.sample(&mut rng).to_string()).unwrap();
+        amount.mul(expected_gain).sub(gas)
+    }
+
+
+
 }
 
 /*
@@ -86,5 +181,57 @@ mod tests {
         let mut contract = Contract::default();
         contract.set_greeting("howdy".to_string());
         assert_eq!(contract.get_greeting(), "howdy");
+    }
+}
+
+#[near(serializers = [json, borsh])]
+#[derive(Debug, Clone, PartialEq)]
+pub struct BigDeDecimal {
+    positive: bool,
+    digits: Vec<u32>,
+    scale: i64
+}
+
+impl Eq for BigDeDecimal {}
+
+impl PartialOrd for BigDeDecimal {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        BigDecimal::from(self).partial_cmp(&BigDecimal::from(other))
+    }
+}
+
+impl Ord for BigDeDecimal {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        BigDecimal::from(self).cmp(&BigDecimal::from(other))
+    }
+}
+
+impl From<BigDecimal> for BigDeDecimal {
+    fn from(value: BigDecimal) -> Self {
+        let (bigint, scale) = value.as_bigint_and_exponent();
+        let (sign, digits) = bigint.to_u32_digits();
+        let positive = sign != Sign::Minus;
+        BigDeDecimal {positive, digits, scale}
+    }
+}
+
+impl From<&BigDecimal> for BigDeDecimal {
+    fn from(value: &BigDecimal) -> Self {
+        value.clone().into()
+    }
+}
+
+impl From<BigDeDecimal> for BigDecimal {
+    fn from(value: BigDeDecimal) -> Self {
+        let BigDeDecimal {positive, digits, scale} = value;
+        let sign = if positive {Sign::Plus} else {Sign::Minus};
+        let bigint = bigdecimal::num_bigint::BigInt::new(sign, digits);
+        BigDecimal::new(bigint, scale)
+    }
+}
+
+impl From<&BigDeDecimal> for BigDecimal {
+    fn from(value: &BigDeDecimal) -> Self {
+        value.clone().into()
     }
 }
--- agent/src/token.rs
+++ agent/src/token.rs
@@ -1,61 +1,49 @@
+use near_sdk::*;
+
+#[near(serializers = [json, borsh])]
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Token {
-    USDC(USDC),
-    USDT(USDT),
-    NEAR(NEAR),
+    NEAR {
+        symbol: String,
+        address: String,
+        decimals: u8,
+    },
+    USDT {
+        symbol: String,
+        address: String,
+        decimals: u8,
+    },
+    USDC {
+        symbol: String,
+        address: String,
+        decimals: u8,
+    },
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct USDC {
-    symbol: &'static str,
-    address: &'static str,
-    decimals: u8,
-}
+use Token::*;
 
-// TODO this is fuckin ugly, surely there is a better way to represent tokens haha
-impl USDC {
-    pub fn new() -> Self {
-        USDC {
-            symbol: "USDC",
-            address: "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
-            //TODO verify
+impl Token {
+    pub fn near() -> Self {
+        NEAR {
+            symbol: "NEAR".to_string(),
+            address: "wrap.near".to_string(),
+            decimals: 24,
+        }
+    }
+
+    pub fn usdt() -> Self {
+        USDT {
+            symbol: "USDT".to_string(),
+            address: "usdt.tether-token.near".to_string(),
             decimals: 6,
         }
     }
-}
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct USDT {
-    symbol: &'static str,
-    address: &'static str,
-    decimals: u8,
-}
-
-impl USDT {
-    pub fn new() -> Self {
-        USDT {
-            symbol: "USDT",
-            address: "usdt.tether-token.near",
-            //TODO verify
-            decimals: 18,
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct NEAR {
-    symbol: &'static str,
-    address: &'static str,
-    decimals: u8,
-}
-
-impl NEAR {
-    pub fn new() -> Self {
-        NEAR {
-            symbol: "NEAR",
-            address: "wrap.near",
-            //TODO verify
-            decimals: 24,
+    pub fn usdc() -> Self {
+        USDC {
+            symbol: "USDC".to_string(),
+            address: "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1".to_string(),
+            decimals: 6,
         }
     }
 }
